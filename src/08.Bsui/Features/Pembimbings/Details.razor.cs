399f881f1213361270ab406cb59bc7ce36208fcd
Fadhil <88182212+FadhilHere@users.noreply.github.com>
2024-11-18 15:25:28 +0700

    setup components dialog edit -> pembimbing

--- src/08.Bsui/Features/Pembimbings/Details.razor.cs
+++ src/08.Bsui/Features/Pembimbings/Details.razor.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using Pertamina.SIMIT.Bsui.Common.Components;
 using Pertamina.SIMIT.Bsui.Common.Constants;
+using Pertamina.SIMIT.Bsui.Common.Extensions;
+using Pertamina.SIMIT.Bsui.Features.Pembimbings.Components;
 using Pertamina.SIMIT.Bsui.Features.Pembimbings.Constants;
 using Pertamina.SIMIT.Shared.Common.Constants;
 using Pertamina.SIMIT.Shared.Common.Responses;
+using Pertamina.SIMIT.Shared.Pembimbings.Commands.UpdatePembimbing;
+using Pertamina.SIMIT.Shared.Pembimbings.Constants;
 using Pertamina.SIMIT.Shared.Pembimbings.Queries.GetPembimbing;
 
 namespace Pertamina.SIMIT.Bsui.Features.Pembimbings;
@@ -68,5 +73,52 @@ public partial class Details
         }
 
         _pembimbing = response.Result!;
+    }
+
+    private async Task ShowDialogEdit()
+    {
+        var request = new UpdatePembimbingRequest
+        {
+            PembimbingId = _pembimbing.Id,
+            Nama = _pembimbing.Nama,
+            Nip = _pembimbing.Nip,
+            Jabatan = _pembimbing.Jabatan
+        };
+
+        var parameters = new DialogParameters
+        {
+            { nameof(DialogEdit.Request), request }
+        };
+
+        var dialog = _dialogService.Show<DialogEdit>($"{CommonDisplayTextFor.Edit} {DisplayTextFor.Pembimbing}", parameters);
+        var result = await dialog.Result;
+
+        if (!result.Cancelled)
+        {
+            _snackbar.AddSuccess(SuccessMessageFor.Action(DisplayTextFor.Pembimbing, _pembimbing.Nama, CommonDisplayTextFor.Updated));
+
+            await Reload();
+        }
+    }
+
+    private async Task ShowDialogDelete()
+    {
+        var dialog = _dialogService.Show<DialogDelete>($"{CommonDisplayTextFor.Delete} {DisplayTextFor.Pembimbing} {_pembimbing.Nama}");
+        var result = await dialog.Result;
+
+        if (!result.Cancelled)
+        {
+            var response = await _pembimbingService.DeletePembimbingAsync(_pembimbing.Id);
+
+            if (response.Error is not null)
+            {
+                _snackbar.AddErrors(response.Error.Details);
+
+                return;
+            }
+
+            _snackbar.AddSuccess(SuccessMessageFor.Action(DisplayTextFor.Pembimbing, _pembimbing.Nama, CommonDisplayTextFor.Deleted));
+            _navigationManager.NavigateTo(RouteFor.Index);
+        }
     }
 }
