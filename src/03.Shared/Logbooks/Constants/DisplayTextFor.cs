b58e4bb25243895e9a2a8d7c6466688613f13ab5
HP <HP@LAPTOP-BOFL1FRN>
2024-11-20 09:58:42 +0700

    Progress Logbook

---
+++ src/03.Shared/Logbooks/Constants/DisplayTextFor.cs
@@ -0,0 +1,7 @@
+﻿namespace Pertamina.SIMIT.Shared.Logbooks.Constants;
+public class DisplayTextFor
+{
+    public DateTime LogbookDate { get; set; } = default!;
+    public string Aktifitas { get; set; } = default!;
+}
+
